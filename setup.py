6496ed49af2722b539d5d9f8ebd6b1c3a53082b0
J <65361824+SentientHousePlant@users.noreply.github.com>
2022-06-14 14:28:45 +0100

    🚀 Added setup.py

---
+++ setup.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = ["fsd_tech"]
+
+package_data = {"": ["*"]}
+
+install_requires = ["PyYAML>=6.0,<7.0", "python-dotenv>=0.20.0,<0.21.0"]
+
+setup_kwargs = {
+    "name": "fsd-tech",
+    "version": "0.1.0",
+    "description": "Utils for the fsd-tech team",
+    "long_description": None,
+    "author": "Version 1",
+    "author_email": None,
+    "maintainer": None,
+    "maintainer_email": None,
+    "url": None,
+    "packages": packages,
+    "package_data": package_data,
+    "install_requires": install_requires,
+    "python_requires": ">=3.10,<4.0",
+}
+
+
+setup(**setup_kwargs)
