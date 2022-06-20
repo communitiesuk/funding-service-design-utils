e9954a39d746b9f12d88a7030bfdc464e486a4df
harryyo <56394038+harryyo@users.noreply.github.com>
2022-06-20 18:19:48 +0100

    Add logging util

--- setup.py
+++ setup.py
@@ -1,22 +1,28 @@
 # -*- coding: utf-8 -*-
 from setuptools import setup
 
-packages = ["fsd_utils"]
+packages = ["fsd_tech"]
 
 package_data = {"": ["*"]}
 
+install_requires = [
+    "Flask==2.0.2",
+    "python-json-logger==2.0.2",
+]
+
 setup_kwargs = {
-    "name": "funding-service-design-utils",
-    "version": "0.0.3",
+    "name": "fsd-tech",
+    "version": "0.2.0",
     "description": "Utils for the fsd-tech team",
     "long_description": None,
-    "author": "DLUHC",
+    "author": "Version 1",
     "author_email": None,
     "maintainer": None,
     "maintainer_email": None,
-    "url": "https://github.com/communitiesuk/funding-service-design-utils",
+    "url": None,
     "packages": packages,
     "package_data": package_data,
+    "install_requires": install_requires,
     "python_requires": ">=3.10,<4.0",
 }
 
