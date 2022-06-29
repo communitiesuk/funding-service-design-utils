d31d97db9c438deda6a60641ea015b15979f34cc
harryyo <56394038+harryyo@users.noreply.github.com>
2022-06-29 12:12:19 +0100

    Add guicorn to fsd_utils __init__.py

--- fsd_utils/__init__.py
+++ fsd_utils/__init__.py
@@ -1,4 +1,5 @@
 from fsd_utils.config.configclass import configclass  # noqa
 from fsd_utils import logging  # noqa
+from fsd_utils import gunicorn  # noqa
 
-__all__ = [configclass, logging]
+__all__ = [configclass, logging, gunicorn]
