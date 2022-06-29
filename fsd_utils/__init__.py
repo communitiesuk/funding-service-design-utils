8f8fb78c62a13d391a3deccae42d998af41c8c17
harryyo <56394038+harryyo@users.noreply.github.com>
2022-06-29 12:06:57 +0100

    Add logging and gunicorn utils

--- fsd_utils/__init__.py
+++ fsd_utils/__init__.py
@@ -1,3 +1,4 @@
 from fsd_utils.config.configclass import configclass  # noqa
+from fsd_utils import logging  # noqa
 
-__all__ = [configclass]
+__all__ = [configclass, logging]
--- setup.py
+++ setup.py
@@ -8,6 +8,8 @@ install_requires = [
     "PyYAML>=6.0,<7.0",
     "python-dotenv>=0.20.0,<0.21.0",
     "rich>=12.4.4,<13.0.0",
+    "Flask==2.0.2",
+    "python-json-logger==2.0.2",
 ]
 
 setup_kwargs = {
