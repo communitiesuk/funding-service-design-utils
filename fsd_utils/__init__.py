b4db448ea3b20dd44a3fc048656290564531e0d9
harryyo <56394038+harryyo@users.noreply.github.com>
2022-06-30 16:29:41 +0100

    Simplified logging imports

--- fsd_utils/__init__.py
+++ fsd_utils/__init__.py
@@ -1,5 +1,5 @@
 from fsd_utils import gunicorn  # noqa
 from fsd_utils.config.configclass import configclass  # noqa
-from fsd_utils.logging import logging  # noqa
+from fsd_utils import logging  # noqa
 
 __all__ = [configclass, logging, gunicorn]
