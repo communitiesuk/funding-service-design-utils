c9c926f39fdf4e4b09034a1acbbb2dd404004e97
harryyo <56394038+harryyo@users.noreply.github.com>
2022-07-11 18:54:06 +0100

    update version number

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ install_requires = [
 
 setup_kwargs = {
     "name": "funding-service-design-utils",
-    "version": "0.0.10",
+    "version": "0.0.9",
     "description": "Utils for the fsd-tech team",
     "long_description": None,
     "author": "DHULC",
