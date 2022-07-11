701b470c277c0cbaabb02906422c3f6c21bcdd66
harryyo <56394038+harryyo@users.noreply.github.com>
2022-07-11 16:01:28 +0100

    Security added to fsd utils __init__

--- fsd_utils/__init__.py
+++ fsd_utils/__init__.py
@@ -1,5 +1,6 @@
 from fsd_utils import gunicorn  # noqa
-from fsd_utils.config.configclass import configclass  # noqa
 from fsd_utils import logging  # noqa
+from fsd_utils import security  # noqa
+from fsd_utils.config.configclass import configclass  # noqa
 
-__all__ = [configclass, logging, gunicorn]
+__all__ = [configclass, logging, gunicorn, security]
