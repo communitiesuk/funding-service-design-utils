1a0aa929111db1574abb3731f9d0c97d7d276aab
harryyo <56394038+harryyo@users.noreply.github.com>
2022-07-11 18:53:23 +0100

    abort with redirect on failed token validation

--- fsd_utils/security/decorators.py
+++ fsd_utils/security/decorators.py
@@ -12,37 +12,37 @@ from jwt import PyJWTError
 
 def _failed_redirect(message: str = "Link expired or invalid"):
     auth_host_key = "AUTHENTICATOR_HOST"
-    try:
-        current_app.config[auth_host_key]
-    except KeyError:
+    authenticator_host = current_app.config.get(auth_host_key)
+
+    if not authenticator_host:
         current_app.logger.critical(
             f"Failed Redirect: {auth_host_key} " "not set in environ"
         )
         abort(500)
 
-    return redirect(
-        f"{current_app.config[auth_host_key]}"
-        "/magic-links/new?error="
-        f"{quote_plus(message)}"
+    return abort(
+        redirect(
+            f"{authenticator_host}"
+            "/magic-links/new?error="
+            f"{quote_plus(message)}"
+        )
     )
 
 
 def _check_access_token():
     user_token_cookie_key = "FSD_USER_TOKEN_COOKIE_NAME"
-    try:
-        current_app.config[user_token_cookie_key]
-    except KeyError:
+    user_token_cookie_name = current_app.config.get(user_token_cookie_key)
+    if not user_token_cookie_name:
         current_app.logger.critical(
             f"Failed Check Token: {user_token_cookie_key} "
             "not set in environ"
         )
         abort(500)
 
-    login_cookie = request.cookies.get(
-        current_app.config[user_token_cookie_key]
-    )
+    login_cookie = request.cookies.get(user_token_cookie_name)
     if not login_cookie:
         _failed_redirect()
+
     try:
         return validate_token_rs256(login_cookie)
     except (PyJWTError, ExpiredSignatureError):
