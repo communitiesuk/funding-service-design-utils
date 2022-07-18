47325a080f1ddf81b69caaef7c447d0153cab29c
Sarah Sloan <sarah.sloan@levellingup.gov.uk>
2022-07-18 10:08:51 +0100

    added more common config, testing flask_root

--- fsd_utils/config/commonconfig.py
+++ fsd_utils/config/commonconfig.py
@@ -1,4 +1,13 @@
 import os
+from pathlib import Path
 
 class CommonConfig:
-    SECRET_KEY = os.getenv("SECRET_KEY")
+
+    # ---------------
+    #  Application Config
+    # ---------------
+    SECRET_KEY = os.getenv("SECRET_KEY", "secret_key")
+    SESSION_COOKIE_NAME = os.getenv("SESSION_COOKIE_NAME", "session_cookie")
+    FLASK_ENV = os.getenv("FLASK_ENV", "development")
+    FLASK_ROOT = str(Path(__file__).parent.parent.parent)
+    print("FLASK_ROOT:", FLASK_ROOT)
