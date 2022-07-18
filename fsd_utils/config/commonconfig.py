754aa2d546857a520af162cdce1d91148cc4f950
Sarah Sloan <sarah.sloan@levellingup.gov.uk>
2022-07-18 10:16:05 +0100

    removing flask_root from common due to file paths

--- fsd_utils/config/commonconfig.py
+++ fsd_utils/config/commonconfig.py
@@ -9,5 +9,3 @@ class CommonConfig:
     SECRET_KEY = os.getenv("SECRET_KEY", "secret_key")
     SESSION_COOKIE_NAME = os.getenv("SESSION_COOKIE_NAME", "session_cookie")
     FLASK_ENV = os.getenv("FLASK_ENV", "development")
-    FLASK_ROOT = str(Path(__file__).parent.parent.parent)
-    print("FLASK_ROOT:", FLASK_ROOT)
