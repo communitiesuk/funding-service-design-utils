184dfff68b976ff5d550ab266f3a3e324ca72f79
Sarah Sloan <sarah.sloan@levellingup.gov.uk>
2022-07-27 15:31:41 +0100

    Setting https to false as not needed on paas

--- fsd_utils/config/commonconfig.py
+++ fsd_utils/config/commonconfig.py
@@ -118,7 +118,7 @@ class CommonConfig:
     ALLOW_FROM = "ALLOW-FROM"
     ONE_YEAR_IN_SECS = 31556926
 
-    FORCE_HTTPS = True
+    FORCE_HTTPS = False
 
     TALISMAN_SETTINGS = {
         "feature_policy": FSD_FEATURE_POLICY,
