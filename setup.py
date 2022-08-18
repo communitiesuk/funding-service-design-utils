2d228b9cac82596822fff178daeb268538b4baa4
Sarah Sloan <sarah.sloan@levellingup.gov.uk>
2022-08-18 12:35:30 +0100

    version bump

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ install_requires = [
 
 setup_kwargs = {
     "name": "funding-service-design-utils",
-    "version": "0.0.13",
+    "version": "0.0.14",
     "description": "Utils for the fsd-tech team",
     "long_description": None,
     "author": "DLUHC",
