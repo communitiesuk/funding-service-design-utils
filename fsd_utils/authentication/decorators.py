e8ba7d754fb6897fa9fa31385a8ef9220f04e867
harryyo <56394038+harryyo@users.noreply.github.com>
2022-09-12 13:48:53 +0100

    Add @login_requested decorator to allow routes to check but not force login, also improved docstrings

--- fsd_utils/authentication/decorators.py
+++ fsd_utils/authentication/decorators.py
@@ -20,24 +20,46 @@ def _failed_redirect():
     return abort(redirect(authenticator_host + signout_route))
 
 
-def _check_access_token():
+def _check_access_token(auto_redirect=True):
+    """
+    Check the expected auth cookie for a
+    valid JWT token.
+    :param auto_redirect: (bool) redirect on failed authentication if True
+    :return:
+        - If a valid JWT token is found then return token claims
+        - If no valid token is found:
+            -- If auto_redirect is True then issue a _failed_redirect()
+            -- If auto_redirect is False then return False
+    """
     user_token_cookie_name = current_app.config[
         config_var_user_token_cookie_name
     ]
 
     login_cookie = request.cookies.get(user_token_cookie_name)
     if not login_cookie:
-        _failed_redirect()
+        if auto_redirect:
+            _failed_redirect()
+        return False
 
     try:
         return validate_token_rs256(login_cookie)
     except (PyJWTError, ExpiredSignatureError):
-        _failed_redirect()
+        if auto_redirect:
+            _failed_redirect()
+        return False
 
 
 def login_required(f):
-    """Execute function if request contains valid JWT
-    and pass account_id to route."""
+    """
+    Execute function if request contains valid JWT
+    and pass account auth params to route as attributes
+    on the flask request global 'g' object:
+        - g.account_id: (str) users account id
+        - g.is_authenticated: (bool) authentication status
+        - g.logout_url: (str) the service sign-out url
+    If no valid auth JWT found then redirect to
+    service config invalid login route
+    """
 
     @wraps(f)
     def decorated(*args, **kwargs):
@@ -46,6 +68,34 @@ def login_required(f):
         g.account_id = token_payload.get("accountId")
         g.is_authenticated = True
         g.logout_url = authenticator_host + signout_route
+        return f(*args, **kwargs)
+
+    return decorated
+
+
+def login_requested(f):
+    """
+    If request contains valid JWT
+    then pass account auth params to route as attributes
+    on the flask request global 'g' object:
+        - g.account_id: (str) users account id
+        - g.is_authenticated: (bool) authentication status
+        - g.logout_url: (str) the service sign-out url
+    If no valid auth JWT found then update the g.is_authenticated
+    variable to False and the g.account_id to None
+    """
+
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        token_payload = _check_access_token(auto_redirect=False)
+        if token_payload and isinstance(token_payload, dict):
+            authenticator_host = current_app.config[config_var_auth_host]
+            g.account_id = token_payload.get("accountId")
+            g.is_authenticated = True
+            g.logout_url = authenticator_host + signout_route
+        else:
+            g.account_id = None
+            g.is_authenticated = False
         return f(*args, **kwargs)
 
     return decorated
