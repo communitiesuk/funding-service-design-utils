1569e789f18e7eedb7b0b1d5e804062c06e862da
srh-sloan <srh-sloan@users.noreply.github.com>
2022-09-15 07:48:09 +0100

    FS-1135 - Publish to PyPI (#25)
    Updated workflow to publish to pypi, and created pyproject.toml

--- setup.py
+++ setup.py
@@ -4,32 +4,9 @@ from setuptools import setup
 
 package_data = {"": ["*"]}
 
-install_requires = [
-    "Flask-Babel>=2.0.0,<3.0.0",
-    "PyYAML>=6.0,<7.0",
-    "python-dotenv>=0.20.0,<0.21.0",
-    "rich>=12.4.4,<13.0.0",
-    "Flask>=2.1.1,<3.0.0",
-    "python-json-logger>=2.0.2,<3.0.0",
-    "gunicorn>=20.1.0,<21.0.0",
-    "pytz>=2022.1",
-    "PyJWT[crypto]>=2.4.0",
-]
-
 setup_kwargs = {
-    "name": "funding-service-design-utils",
-    "version": "0.0.21",
-    "description": "Utils for the fsd-tech team",
-    "long_description": None,
-    "author": "DLUHC",
-    "author_email": None,
-    "maintainer": None,
-    "maintainer_email": None,
     "packages": find_packages(),
-    "url": "https://github.com/communitiesuk/funding-service-design-utils",
     "package_data": package_data,
-    "install_requires": install_requires,
-    "python_requires": ">=3.10,<4.0",
 }
 
 setup(**setup_kwargs)
