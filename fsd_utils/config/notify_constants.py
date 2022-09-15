a94a15a78efa854f9bda2620596e7ba6ef8b408d
Ram <80714392+ramsharma-prog@users.noreply.github.com>
2022-09-15 11:03:55 +0100

    Application fields updated

--- fsd_utils/config/notify_constants.py
+++ fsd_utils/config/notify_constants.py
@@ -1,6 +1,9 @@
 class NotifyConstants:
 
-    # ---- Required key Fields (DO NOT CHANGE) ----#
+    # ---- Notification service endpoint ----#
+    NOTIFICATION_SERVICE_ENDPOINT = "/send"
+
+    # ---- Required keys Fields (DO NOT CHANGE) ----#
     FIELD_TYPE = "type"  # for TEMPLATE_TYPE.
     FIELD_CONTENT = "content"  # for contents to be shared.
     FIELD_TO = "to"  # for contact_info (do not set in common config)
@@ -8,9 +11,6 @@ class NotifyConstants:
     # ---- Template types constants (DO NOT CHANGE) ---#
     TEMPLATE_TYPE_MAGIC_LINK = "MAGIC_LINK"
     TEMPLATE_TYPE_APPLICATION = "APPLICATION_RECORD_OF_SUBMISSION"
-    TEMPLATE_TYPE_NOTIFICATION = "NOTIFICATION"
-    TEMPLATE_TYPE_REMINDER = "REMINDER"
-    TEMPLATE_TYPE_AWARD = "AWARD"
 
     # Magic_Link Fields for FIELD_CONTENT #
     MAGIC_LINK_CONTACT_HELP_EMAIL_FIELD = "contact_help_email"
@@ -19,7 +19,12 @@ class NotifyConstants:
     MAGIC_LINK_FUND_NAME_FIELD = "fund_name"
 
     # Application Fields for FIELD_CONTENT #
-    APPLICATION_SUBMISSION_DATE_FIELD = "date_submitted"
-    APPLICATION_FUND_NAME_FIELD = "project_name"
-    APPLICATION_ROUND_NAME_FIELD = "round_id"
-    APPLICATION_ID_FIELD = "id"
+    # APPLICATION_SUBMISSION_DATE_FIELD = "date_submitted"
+    # APPLICATION_FUND_NAME_FIELD = "project_name"
+    # APPLICATION_ROUND_NAME_FIELD = "round_id"
+    # APPLICATION_ID_FIELD = "id"
+
+    APPLICATION_FIELD = "application"
+    APPLICATION_FORMS_FIELD = "forms"
+    APPLICATION_NAME_FIELD = "name"
+    APPLICATION_QUESTIONS_FIELD = "questions"
