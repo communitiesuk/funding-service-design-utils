246412ef3b006d60aa9553bccb0ed6c9ee18aae6
Gideon Goldberg <1764158+gidsg@users.noreply.github.com>
2022-11-01 12:18:47 +0000

    filter healthcheck endpoint from transaction sampling

--- fsd_utils/sentry/init_sentry.py
+++ fsd_utils/sentry/init_sentry.py
@@ -5,6 +5,16 @@ from fsd_utils import CommonConfig
 from sentry_sdk.integrations.flask import FlaskIntegration
 
 
+def _traces_sampler(sampling_context):
+    wsgi_environ = sampling_context.get("wsgi_environ")
+    if wsgi_environ and wsgi_environ.get("PATH_INFO") == "/healthcheck":
+        # Drop this transaction, by setting its sample rate to 0%
+        return 0
+    else:
+        # Default sample rate for all others (replaces traces_sample_rate)
+        return 0.1
+
+
 def init_sentry():
     if getenv("SENTRY_DSN"):
         sentry_sdk.init(
@@ -12,6 +22,6 @@ def init_sentry():
             integrations=[
                 FlaskIntegration(),
             ],
-            traces_sample_rate=0.1,
+            traces_sampler=_traces_sampler,
             release=getenv("GITHUB_SHA"),
         )
