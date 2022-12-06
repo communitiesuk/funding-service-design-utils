881143d1c5693f3fca8f11ece7cab757aab83896
harryyo <56394038+harryyo@users.noreply.github.com>
2022-12-06 12:26:23 +0000

    Add azure user details and azure_ad_role_map

--- fsd_utils/authentication/decorators.py
+++ fsd_utils/authentication/decorators.py
@@ -12,6 +12,7 @@ from jwt import PyJWTError
 from .config import config_var_auth_host
 from .config import config_var_user_token_cookie_name
 from .config import signout_route
+from .utils import get_highest_role
 
 
 def _failed_redirect():
@@ -66,6 +67,10 @@ def login_required(f):
         token_payload = _check_access_token()
         authenticator_host = current_app.config[config_var_auth_host]
         g.account_id = token_payload.get("accountId")
+        g.user.full_name = token_payload.get("fullName")
+        g.user.email = token_payload.get("email")
+        g.user.roles = token_payload.get("roles")
+        g.user.highest_role = get_highest_role(g.user.roles)
         g.is_authenticated = True
         g.logout_url = authenticator_host + signout_route
         return f(*args, **kwargs)
@@ -92,6 +97,10 @@ def login_requested(f):
         g.logout_url = authenticator_host + signout_route
         if token_payload and isinstance(token_payload, dict):
             g.account_id = token_payload.get("accountId")
+            g.user.full_name = token_payload.get("fullName")
+            g.user.email = token_payload.get("email")
+            g.user.roles = token_payload.get("roles")
+            g.user.highest_role = get_highest_role(g.user.roles)
             g.is_authenticated = True
         else:
             g.account_id = None
--- fsd_utils/authentication/utils.py
+++ fsd_utils/authentication/utils.py
@@ -1,6 +1,9 @@
+from typing import List
+
 import jwt as jwt
 from flask import current_app
 
+from .config import azure_ad_role_mapping
 from .config import config_var_rs256_public_key
 
 
@@ -11,3 +14,9 @@ def _validate_token(token, key, algorithms):
 def validate_token_rs256(token):
     key = current_app.config.get(config_var_rs256_public_key)
     return _validate_token(token, key, algorithms=["RS256"])
+
+
+def get_highest_role(roles: List[str]):
+    for _, role in azure_ad_role_mapping:
+        if role in roles:
+            return role
