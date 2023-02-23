e942558b7f66849b2bf1ec30331d967eee505f3f
Gio Karunakaran <gio.karunakaran@version1.com>
2023-02-23 14:53:29 +0000

    fs-2277-toggles

--- fsd_utils/toggles/toggles.py
+++ fsd_utils/toggles/toggles.py
@@ -1,24 +1,19 @@
-import redis
 from flipper import FeatureFlagClient, RedisFeatureFlagStore
 from flask_redis import FlaskRedis
-from fsd_utils.healthchecks.checkers import RedisChecker
 
 redis_store = FlaskRedis()
 
-
-r = redis.Redis(host='localhost', port=6379, db=0)
-
 # Add feature flagging using Redis store
 store = RedisFeatureFlagStore(redis_store, base_key='feature')
-features = FeatureFlagClient(store)
+client = FeatureFlagClient(store)
 
 feature_configuration = {
-    "FLAGGING": True,
+    "FLAGGING": False,
     "COMMENTING": True,
     "REMINDERS": False
 }
 
 for feature, toggle in feature_configuration.items():
-    features.create(feature)
+    client.create(feature)
     if toggle:
-        features.enable(feature)
+        client.enable(feature)
