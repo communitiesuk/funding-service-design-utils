63e90cec96058245263d6502963fc3b411f6cbda
Sarah Sloan <sarah.sloan@levellingup.gov.uk>
2023-03-16 13:36:01 +0000

    Adding docs

--- fsd_utils/fixtures/db_fixtures.py
+++ fsd_utils/fixtures/db_fixtures.py
@@ -6,8 +6,12 @@ from sqlalchemy_utils.functions import drop_database
 
 
 def prep_db(reuse_db=False):
-    """Provide the transactional fixtures with access to the database via a
-    Flask-SQLAlchemy database connection."""
+    """
+    Determines whether the target database already exists, if it
+    doesn't it is created.
+    If it exists but reuse_db is False, then the DB is dropped and recreated.
+    upgrade() is always run to make sure the schema is up to date.
+    """
 
     from config import Config
 
@@ -15,11 +19,9 @@ def prep_db(reuse_db=False):
     refresh_db = not reuse_db
 
     if no_db:
-
         create_database(Config.SQLALCHEMY_DATABASE_URI)
 
     elif refresh_db:
-
         drop_database(Config.SQLALCHEMY_DATABASE_URI)
         create_database(Config.SQLALCHEMY_DATABASE_URI)
 
@@ -28,11 +30,21 @@ def prep_db(reuse_db=False):
 
 @pytest.fixture(scope="session")
 def _db(app, request):
+    """
+    Fixture to supply tests with direct access to the database
+    """
     yield app.extensions["sqlalchemy"]
 
 
 @pytest.fixture(scope="session")
 def recreate_db(request, _db, app):
+    """
+    Fixture to determine whether we need to clear out the existing DB
+    (based on cache values) and then call prep_db.
+    This is session scoped so we only drop/create the db
+    at the start of a test session
+    """
+    print("Recreating db")
     reuse_db = bool(request.config.cache.get("reuse_db", False))
     with app.app_context():
         prep_db(reuse_db)
@@ -41,17 +53,20 @@ def recreate_db(request, _db, app):
 
 
 @pytest.fixture(scope="module")
-def clear_test_data(app, _db, request):
+def clear_test_data(app, _db, request, recreate_db):
     """
     Fixture to clean up the database after each test.
 
-    This fixture clears the database by deleting all data
-    from tables and disabling foreign key checks before the test,
-    and resetting foreign key checks after the test.
+    This fixture reads preserve_test_data from the cache
+    (see enable_preserve_test_data below),
+    and if we are not preserving the data it clears the database
+    by deleting all data from all tables.
+
+    This is module scoped so that each test file gets a fresh empty
+    database.
 
     """
     with app.app_context():
-
         yield
         preserve_test_data = request.config.cache.get(
             "preserve_test_data", None
@@ -76,6 +91,13 @@ def clear_test_data(app, _db, request):
 
 @pytest.fixture(scope="function")
 def enable_preserve_test_data(request):
+    """
+    Fixture to read the markers on a test and if preserve_test_data is
+    set, it sets it in the cache.
+    This can't be combined with clear_test_data due to conflicts on the fixture
+    scope, so this function reads the test markers and clear_test_data
+    reads the cache.
+    """
     marker = request.node.get_closest_marker("preserve_test_data")
     if marker is not None:
         request.config.cache.set("preserve_test_data", True)
