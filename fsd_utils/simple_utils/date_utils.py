ebe10483df9db0798c8fd484476cd72c490fbece
Gio Karunakaran <gio.karunakaran@version1.com>
2023-06-09 13:57:52 +0100

    fs-2892-date-logic

--- fsd_utils/simple_utils/date_utils.py
+++ fsd_utils/simple_utils/date_utils.py
@@ -1,13 +1,27 @@
 from datetime import datetime
 
+import pytz
+
 
 def current_datetime_after_given_iso_string(value: str) -> bool:
-    today = datetime.today().now()
+    # Grab local timezone
+    uk_tz = pytz.timezone('Europe/London')
+    # Grab current localised datetime
+    now_datetime = datetime.now(uk_tz)
+    # Convert now_datetime to offset-naive
+    now_datetime_naive = now_datetime.replace(tzinfo=None)
+    # Convert value datetime to correct format for camparison
     parsed = datetime.fromisoformat(value)
-    return today > parsed
+    return now_datetime_naive > parsed
 
 
 def current_datetime_before_given_iso_string(value: str) -> bool:
-    today = datetime.today().now()
+    # Grab local timezone
+    uk_tz = pytz.timezone('Europe/London')
+    # Grab current localised datetime
+    now_datetime = datetime.now(uk_tz)
+    # Convert now_datetime to offset-naive
+    now_datetime_naive = now_datetime.replace(tzinfo=None)
+    # Convert value datetime to correct format for camparison
     parsed = datetime.fromisoformat(value)
-    return today < parsed
+    return now_datetime_naive < parsed
