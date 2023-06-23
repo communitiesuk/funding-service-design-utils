6fb5ebac9a715fe0e27987facd682f0cfa4347fb
tferns <thomas.ferns@version1.com>
2023-06-23 16:43:30 +0100

    FS-2990: Change highest role to map

--- fsd_utils/authentication/utils.py
+++ fsd_utils/authentication/utils.py
@@ -1,4 +1,5 @@
 from typing import List
+from typing import Mapping
 
 import jwt as jwt
 from flask import current_app
@@ -16,8 +17,28 @@ def validate_token_rs256(token):
     return _validate_token(token, key, algorithms=["RS256"])
 
 
-def get_highest_role(roles: List[str]):
-    if roles and len(roles) > 0:
-        for _, role in azure_ad_role_map.items():
-            if role in roles:
-                return role
+_ROLE_HIERARCHY = [
+    "LEAD_ASSESSOR",
+    "ASSESSOR",
+    "COMMENTER",
+]
+
+
+def get_highest_role_map(roles: list[str]) -> Mapping[str, str]:
+    roles_with_fund_prefix = tuple(f"_{rh}" for rh in _ROLE_HIERARCHY)
+    filtered_roles = [r for r in roles if r.endswith(roles_with_fund_prefix)]
+
+    fund_short_name_to_roles_list = {}
+    for role in filtered_roles:
+        fund_short_name, sub_role = role.split("_", 1)
+        if fund_short_name in fund_short_name_to_roles_list:
+            fund_short_name_to_roles_list[fund_short_name].append(sub_role)
+        else:
+            fund_short_name_to_roles_list[fund_short_name] = [sub_role]
+
+    fund_short_name_to_highest_role = {}
+    for fund_short_name, roles_list in fund_short_name_to_roles_list.items():
+        highest_role, *_ = sorted(roles_list, key=lambda x: _ROLE_HIERARCHY.index(x))
+        fund_short_name_to_highest_role[fund_short_name] = highest_role
+
+    return fund_short_name_to_highest_role
