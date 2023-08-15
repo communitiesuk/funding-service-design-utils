3fe0f4eeb4e0acb480b769426b7f6f04a909dbd0
Sarah Sloan <sarah.sloan@levellingup.gov.uk>
2023-08-15 17:03:10 +0100

    adding fund/round ids to useful config

--- fsd_test_utils/test_config/useful_config.py
+++ fsd_test_utils/test_config/useful_config.py
@@ -2,3 +2,7 @@ class UsefulConfig:
     COF_FUND_ID = "47aef2f5-3fcb-4d45-acb5-f0152b5f03c4"
     COF_ROUND_2_ID = "c603d114-5364-4474-a0c4-c41cbf4d3bbd"
     COF_ROUND_2_W3_ID = "5cf439bf-ef6f-431e-92c5-a1d90a4dd32f"
+    COF_ROUND_3_W1_ID = "e85ad42f-73f5-4e1b-a1eb-6bc5d7f3d762"
+
+    NSTF_FUND_ID = "13b95669-ed98-4840-8652-d6b7a19964db"
+    NSTF_ROUND_2_ID = "fc7aa604-989e-4364-98a7-d1234271435a"
