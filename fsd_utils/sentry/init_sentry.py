9523f48919052fe710655f6e636fba01bb210186
Gideon Goldberg <1764158+gidsg@users.noreply.github.com>
2023-10-05 10:01:55 +0100

    separate out profiles sample rate

--- fsd_utils/sentry/init_sentry.py
+++ fsd_utils/sentry/init_sentry.py
@@ -5,7 +5,7 @@ from fsd_utils import CommonConfig
 from sentry_sdk.integrations.flask import FlaskIntegration
 
 
-def _traces_sampler(sampling_context):
+def _sampler(sampling_context, sample_rate):
     wsgi_environ = sampling_context.get("wsgi_environ")
     if wsgi_environ and (
         wsgi_environ.get("PATH_INFO") == "/healthcheck"
@@ -15,7 +15,17 @@ def _traces_sampler(sampling_context):
         return 0
     else:
         # Default sample rate for all others (replaces traces_sample_rate)
-        return float(getenv("SENTRY_TRACES_SAMPLE_RATE", "0.1"))
+        return float(sample_rate)
+
+
+def _profiles_sampler(sample_context):
+    sample_rate = float(getenv("SENTRY_PROFILES_SAMPLE_RATE", "0.02"))
+    _sampler(sample_context, sample_rate=sample_rate)
+
+
+def _traces_sampler(sample_context):
+    sample_rate = float(getenv("SENTRY_TRACES_SAMPLE_RATE", "0.02"))
+    _sampler(sample_context, sample_rate=sample_rate)
 
 
 def init_sentry():
@@ -26,7 +36,7 @@ def init_sentry():
                 FlaskIntegration(),
             ],
             traces_sampler=_traces_sampler,
-            profiles_sampler=_traces_sampler,
+            profiles_sampler=_profiles_sampler,
             release=getenv("GITHUB_SHA"),
         )
 
