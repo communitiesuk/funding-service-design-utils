0dc6367824f69fe3ea0eb23341e616bd9da98475
Sarah Sloan <sarah.sloan@levellingup.gov.uk>
2024-01-11 09:38:12 +0000

    fs-3964 make set language cookie a function

--- fsd_utils/locale_selector/set_lang.py
+++ fsd_utils/locale_selector/set_lang.py
@@ -1,11 +1,28 @@
 from flask import current_app
 from flask import make_response
 from flask import redirect
-from flask import request
+from flask import request, Response
 from fsd_utils import CommonConfig
 
 
 class LanguageSelector:
+
+    @staticmethod
+    def get_cookie_domain(cookie_domain):
+        if not cookie_domain:
+            return None
+        else:
+            return cookie_domain
+
+    @staticmethod
+    def set_language_cookie(locale:str, response:Response):
+        response.set_cookie(
+            CommonConfig.FSD_LANG_COOKIE_NAME,
+            locale,
+            domain=LanguageSelector.get_cookie_domain(current_app.config["COOKIE_DOMAIN"]),
+            max_age=86400 * 30,  # 30 days
+        )
+
     def __init__(self, app):
         self.flask_app = app
         self.flask_app.add_url_rule(
@@ -14,20 +31,10 @@ class LanguageSelector:
 
     @staticmethod
     def select_language(locale):
-        def get_cookie_domain(cookie_domain):
-            if not cookie_domain:
-                return None
-            else:
-                return cookie_domain
 
         response = make_response(
             redirect(request.referrer or request.args.get("return_url") or "/", 302)
         )
-        response.set_cookie(
-            CommonConfig.FSD_LANG_COOKIE_NAME,
-            locale,
-            domain=get_cookie_domain(current_app.config["COOKIE_DOMAIN"]),
-            max_age=86400 * 30,  # 30 days
-        )
+        LanguageSelector.set_language_cookie(locale, response)
 
         return response
