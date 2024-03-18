e1de80efa99ac0c7d6e3eac67927a8ea81a63923
Adam Davies <adam.davies@version1.com>
2024-03-18 10:49:10 +0000

    Fixing flake8 error

--- tests/test_data_utils.py
+++ tests/test_data_utils.py
@@ -275,7 +275,8 @@ test_data_sort_questions_answers = {
             "Revenue for 1 April 2024 to 31 March 2025": "4020",
             "Capital for 1 April 2023 to 31 March 2024": "1230",
             "Capital for 1 April 2024 to 31 March 2025": "1230",
-            "Which membership organisations are you a member of?": "Homeless link",
+            "Which membership organisations are you a member of?": 
+            "Homeless link",
             "When did you start providing day provision?": "March 2023",
             "Revenue costs": "Test Funding Required NS Form \n      . 40\n      . 1 April 2023 to 31 March 2024\n      . Not provided",  # noqa
             "Capital costs": "Test Funding Required NS Form \n      . 50\n      . 1 April 2024 to 31 March 2025",
